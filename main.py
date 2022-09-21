21f14f0c643d8c0037b993aaf4c4e10269718bd8
RomanKiado-12 <820418@mystma.org>
2022-09-21 14:33:42 -0500

    2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def close10(x, y):
-    if abs(x-10) > abs(y-10):
+    if abs(x-10) < abs(y-10):
         return x
-    elif abs(x-10) < abs(y-10):
+    elif abs(x-10) > abs(y-10):
         return y
     else:
         return 0
